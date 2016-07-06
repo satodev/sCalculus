51579acf0459e4f06bb3d3a1f7f67213967b1ae5
satodev <hemmi.satoru@gmail.com>
2016-07-06 22:14:13 +0200

    Authentification System Init

--- server/mongo.js
+++ server/mongo.js
@@ -1,25 +1,57 @@
-const 	express = require('express'),
-	mg = require('mongoose'),
-	user = 'sato',
-	mdp = "sat";
-
-//DefineSchema
-var UserSchema = mg.Schema({
-	name : String,
-	email : String,
-	password : String,
-	date: {type:Date, default:Date.now}
-});
-
-//DefineModel
-var mgUser = mg.model('mgUser', UserSchema);
-
-//defineExport obj
-m = {
-	u : user,
-	md : mdp,
-	mgU : mgUser
-}
+const 	express = require("express"),
+	crypto = require("crypto"),
+	mg = require("mongoose"),
+	user = "sato",
+	mdp = "sat",
+	schema = mg.Schema({
+		name : String,
+		password: String,
+		date:{type:Date, default:Date.now}
+	});
+	//defineExport obj
+	m = {
+		//var
+		u : user,
+		md : mdp,
+		mgU : null,
+		pwd : "password!d?45",
+		//init 
+		initUserSchema : ()=>{
+			m.defineUModel();
+		},
+		//define
+		defineUModel: ()=>{
+			m.mgU = mg.model("mgUser", schema);	
+		},
+		//utils
+		connect : ()=>{
+			mg.connect("mongodb://"+m.u+":"+m.md+"@ds011735.mlab.com:11735/scalculus");
+		},
+		disconnect: ()=>{
+			mg.connection.close();
+		},
+		insertSub : (user, mdp)=>{
+			if(user && mdp){
+				m.connect();
+				m.mgU.find({name: user}, (err, userFound)=>{
+					if(err) console.log(err);
+					if(userFound.length > 0){
+						console.log("user does already exists, check another one");	
+						m.disconnect();
+					}else{
+						m.mgU.create({name:user, password:m.Hmac(mdp)}, (err,user)=>{
+							if(err) console.log(err);
+							console.log('user created');
+							m.disconnect();
+						});
+					}
+				});
+			}
+		},
+		Hmac : (password)=>{
+			return crypto.createHmac('sha256', m.pwd).update(password).digest('hex');
+		}
+	}
 
 //exports
 module.exports = m;
--- views/js/script.js
+++ views/js/script.js
@@ -1,9 +1,31 @@
 var app = angular.module('scalculus', []);
-app.controller('sCalCtrl', ['$scope', '$http',($scope, $http)=>{
+app.controller('sCalCtrl', ['$scope', '$http', 'login','subscribe',($scope, $http, login, subscribe)=>{
+	$scope.passwordConfirm = function(){
+		if($scope.userPwd && $scope.userPwd.length > 3){
+			$scope.stateSub = 'Correct Password Format';
+			$scope.passConf = true;
+		}else{
+			$scope.stateSub = ' Password not long enough';
+			$scope.passConf = false;
+		}
+	}
 	$scope.subscribe = function(){
-		console.log($scope.userName, $scope.userPwd, $scope.vuserPwd);
+		if($scope.userName && $scope.userPwd == $scope.vuserPwd){
+			$scope.stateSub = ' OK ';
+			subscribe.defVar('user', $scope.userName);
+			subscribe.defVar('pwd', $scope.userPwd);
+			subscribe.sendData();
+			$scope.userName = "";
+			$scope.userPwd = "";
+			$scope.vuserPwd = "";
+		}else{
+			$scope.stateSub = 'Enter the same password or enter userName';
+		}
 	}
 	$scope.login = function(){
+		login.setU($scope.logUser);
+		login.setP($scope.logPwd);
+		login.sendServer();
 		if($scope.logUser == "Sato" && $scope.logPwd == "pwd"){
 			$scope.loginStatus = "true";
 		}else{
@@ -13,3 +35,84 @@ app.controller('sCalCtrl', ['$scope', '$http',($scope, $http)=>{
 	var grid = document.getElementById('grid');
 	grid.innerHTML = "Hello, i've been catched";
 }]);
+app.factory('subscribe', ($http)=>{
+	var sub = {
+		user : null,
+		pwd : null,
+		defineVar: (data, value)=>{
+			if(sub.hasOwnProperty(data)){
+				sub[data] = value;
+				console.log(sub[data]);
+			}
+		},
+		sendData : ()=>{
+			if(sub.user && sub.pwd){
+				data = {
+					pseudo: sub.user,
+					pwd : sub.pwd
+				}
+				$http.post('/sub', data).then(
+						success = (e)=>{
+							console.log('User registered', e);
+						},
+						fail = (e)=>{
+							console.log(e.data);
+						}
+						);
+			}
+		}
+	};
+	return {
+		defVar : (prop, value)=>{
+			sub.defineVar(prop,value);	
+		},
+		sendData: ()=>{
+			sub.sendData();
+		}
+	}
+});
+app.factory('login', ($http)=>{
+	var login = {
+		user : null,
+		pwd : null,
+		setUser: (user)=>{
+			if(user){
+				login.user = user;
+			}
+		},
+		setPwd: (pwd)=>{
+			if(pwd){
+				login.pwd =  pwd;	
+			}
+		},
+		sendData: ()=>{
+			if(login.user && login.pwd){
+				//send data to server and wait for response	
+				console.log(login.user ,login.pwd);
+				data = {
+					pseudo : login.user,
+					pwd : login.pwd
+				}
+				$http.post('/login', data).then(
+						success = (e)=>{
+							console.log(e.data.pseudo, e.data.pwd);		
+						},
+						fail = (e)=>{
+							console.log(e.data);
+						}
+						);
+			}
+		}
+	};
+	return {
+		setU :(user)=>{
+			login.setUser(user);
+		},
+		setP :(pwd)=>{
+			login.setPwd(pwd);
+		},
+		sendServer :()=>{
+			login.sendData();
+		}
+	};
+});
